7f05b1dea2a3cc82f182c4a5702d451e55f298c6
thefigtree <ldj8431@naver.com>
2024-10-01 23:48:38 +0900

    func: autoGlow

--- src/components/NoteCard.jsx
+++ src/components/NoteCard.jsx
@@ -1,19 +1,47 @@
+import { useEffect, useRef } from "react";
+import Trash from "../icons/Trash";
+
 const NoteCard = ({ note }) => {
   const body = JSON.parse(note.body);
   const position = JSON.parse(note.position);
   const colors = JSON.parse(note.colors);
 
+  const textAreaRef = useRef(null);
+
+  useEffect(() => {
+    autoGrow(textAreaRef);
+  }, []);
+
+  const autoGrow = (textarea) => {
+    const { current } = textAreaRef;
+    current.style.height = "auto";
+    current.style.height = current.scrollHeight + "px";
+  };
+
   return (
-    <div className="card" style={{ backgroundColor: colors.colorBody }}>
+    <div
+      className="card"
+      style={{
+        backgroundColor: colors.colorBody,
+        left: `${position.x}px`,
+        top: `${position.y}px`,
+      }}
+    >
       <div
         className="card-header"
         style={{ backgroundColor: colors.colorHeader }}
-      ></div>
+      >
+        <Trash></Trash>
+      </div>
 
       <div className="card-body">
         <textarea
+          ref={textAreaRef}
           style={{ color: colors.colorText }}
           defaultValue={body}
+          onInput={() => {
+            autoGrow(textAreaRef);
+          }}
         ></textarea>
       </div>
       {body}
